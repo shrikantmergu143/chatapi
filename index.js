f7ff0845f99526066aa4d5a5533fcccefbfab35a
shrikantmergu143 <shriakntmergu143@gmail.com>
2023-04-20 23:19:32 +0530

    pushing

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
 import express from "express";
+import http from "http"
+import cors  from "cors"
+const authRoutes = require("./routes/authRoutes");
+const mongoose = require('mongoose');
+const socketServer = require('./socketServer')
+require('dotenv').config();
+
+const PORT = process.env.PORT || process.env.API_PORT;
+const HOSTNAME = '0.0.0.0'
 
 const app = express();
-const port = 9000;
+app.use(express.json());
+const corsOptions = {
+  methods: ['GET', 'POST', 'PUT'],
+  origin:"*",
+};
+app.use(cors(corsOptions));
+//Register
+app.use("/api/auth", authRoutes);
+
+const server = http.createServer(app, (req, res)=>{
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+  res.end();
+});
+socketServer.registerSocketServer(server);
+
+mongoose.set('strictQuery', false);
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true }).then(() => {
+    server.listen(PORT, HOSTNAME, ()=>{
+        console.log("Serverport", PORT);
+    });
+}).catch(error => console.error(error));
 app.use("/", (req, res) => {
   res.json({ message: "Hello From Express App" });
 });
 
-app.listen(9000, () => {
-  console.log(`Starting Server on Port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Starting Server on Port ${PORT}`);
 });
